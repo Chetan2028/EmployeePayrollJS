d8826411de8351dd40c5d44a6c718879b68fbe62
Chetan2028 <bmchetan2028@gmail.com>
2020-12-07 17:17:46 +0530

    [Chetan] Refactor : refactored to compute employee wage using function

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -34,3 +34,22 @@ switch(empCheck){
 
 let salary = empHrs*WORKING_RATE_PER_HOUR;
 console.log("UC2 - Employee Salary : " + salary);
+
+//UC3 --> Calcluate Employee Daily Wage using Functions
+
+function getDailyWage(empCheck){
+    switch(empCheck){
+        case IS_PART_TIME :
+            return PART_TIME_HOURS;
+        case IS_FULL_TIME:
+            return FULL_TIME_HOURS;
+        default:
+            return 0;
+    }
+}
+
+empHrs = 0;
+empCheck = Math.floor(Math.random() * 10) % 2;
+empHrs = getDailyWage(empCheck);
+let empWage = empHrs * WORKING_RATE_PER_HOUR;
+console.log("UC3 - Employee Salary : " + empWage);
